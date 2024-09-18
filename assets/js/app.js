7a2dd64af9b441d4956db71de19cdcad512fe1ef
Biplab Keshari Mohanty <167736429+BIPLABx019@users.noreply.github.com>
2024-09-18 19:29:22 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -345,6 +345,31 @@ export const updateWeather = function (lat, lon) {
               container.classList.add("fade-in");
 
           });
+                  const options = {
+            // Required: API key
+            key: 'mNp6Kq9dq9mDEQx9wnQOCWckLblfhKkz', // REPLACE WITH YOUR KEY !!!
+        
+            // Put additional console output
+            verbose: true,
+        
+            // Optional: Initial state of the map
+            lat: 50.4,
+            lon: 14.3,
+            zoom: 5,
+            };
+        // Initialize Windy API
+            windyInit(options, windyAPI => {
+            // windyAPI is ready, and contain 'map', 'store',
+            // 'picker' and other usefull stuff
+        
+            const { map } = windyAPI;
+            // .map is instance of Leaflet map
+        
+            L.popup()
+                .setLatLng([lat, lon])
+                .setContent(`${name}`)
+                .openOn(map);
+            });
     });
 
 
