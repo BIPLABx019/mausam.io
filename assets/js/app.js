df5326cb73e01f97642b21d2e371fceb4fff7045
Biplab Keshari Mohanty <167736429+BIPLABx019@users.noreply.github.com>
2024-09-18 20:15:44 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -355,7 +355,7 @@ export const updateWeather = function (lat, lon) {
             // Optional: Initial state of the map
             lat: parseFloat(lat.slice(4)),
             lon: parseFloat(lon.slice(4)),
-            zoom: 5,
+            zoom: 10,
             };
         // Initialize Windy API
             windyInit(options, windyAPI => {
