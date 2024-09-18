dbb9b19d2abb5effa094b279ce8d250309df6a4c
Biplab Keshari Mohanty <167736429+BIPLABx019@users.noreply.github.com>
2024-09-18 18:00:53 +0530

    Add files via upload

---
+++ assets/js/map.js
@@ -0,0 +1,27 @@
+
+const options = {
+    // Required: API key
+    key: 'mNp6Kq9dq9mDEQx9wnQOCWckLblfhKkz', // REPLACE WITH YOUR KEY !!!
+
+    // Put additional console output
+    verbose: true,
+
+    // Optional: Initial state of the map
+    lat: 50.4,
+    lon: 14.3,
+    zoom: 5,
+};
+
+// Initialize Windy API
+windyInit(options, windyAPI => {
+    // windyAPI is ready, and contain 'map', 'store',
+    // 'picker' and other usefull stuff
+
+    const { map } = windyAPI;
+    // .map is instance of Leaflet map
+
+    L.popup()
+        .setLatLng([50.4, 14.3])
+        .setContent('Hello World')
+        .openOn(map);
+});
